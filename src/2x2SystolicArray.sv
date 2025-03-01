c9dd996a34e97263f5197438c57ea8d583492e3c
Aditya Minocha <adityaminocha10@gmail.com>
2025-03-01 16:33:29 +0530

    Update 2x2SystolicArray.sv

--- src/2x2SystolicArray.sv
+++ src/2x2SystolicArray.sv
@@ -6,6 +6,7 @@ module tt_um_systolic_matmul (
     input  wire [7:0] uio_in,
     output reg  [7:0] uio_out,
     output reg  [7:0] uio_oe,
+    input wire        ena,
     input  wire       clk,
     input  wire       rst_n
 );
